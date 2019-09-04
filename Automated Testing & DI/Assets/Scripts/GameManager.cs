663417ea4584a0647caa0e522e4f497723184389
Sami Koivisto <sami.koivisto.dev@gmail.com>
2019-09-04 17:42:57 +0300

    Removed Singleton from project

--- Automated Testing & DI/Assets/Scripts/GameManager.cs
+++ Automated Testing & DI/Assets/Scripts/GameManager.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using Zenject;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : IInitializable
 {
-
+    public void Initialize()
+    {
+        Debug.Log("GameManager initialized.");
+    }
 }

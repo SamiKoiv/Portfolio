ea4388d6a350c6096f77261e564cf3f51645a312
Sami Koivisto <sami.koivisto.dev@gmail.com>
2019-09-09 16:57:53 +0300

    Integrated Moq

---
+++ Automated Testing & DI/Assets/Scripts/Data/GameParameters.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// GameParameters is a configuration file used by Designers.
+
+[CreateAssetMenu (fileName = "GameParameters", menuName = "Data/GameParameters")]
+public class GameParameters : ScriptableObject
+{
+    
+}
---
+++ Automated Testing & DI/Assets/Scripts/Data/ItemDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Data/ItemDatabase")]
+public class ItemDatabase : ScriptableObject, IItemDatabase
+{
+    [SerializeField] Item[] items = new Item[0];
+
+    public Item GetItem(int id)
+    {
+        return items[id];
+    }
+}
